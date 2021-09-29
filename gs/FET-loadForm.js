60ec33e915e42aae9f6887e98231c55dcb023b75
Brandt Hughes <brandt@brandthughes.com>
2021-09-28 22:21:42 -0500

    split out menu creation
    removed automatic menu item creation so that it can be in its own file

--- gs/FET-loadForm.js
+++ gs/FET-loadForm.js
@@ -1,5 +1,5 @@
 // ░░░░░░░░░▓ LOADS SIDE-BAR FORM WHEN MENU ITEM IS SELECTED
-function loadForm() {
+function footageEntryTool() {
 	const htmlForSidebar = HtmlService.createTemplateFromFile("FET-sidebarForm");
 	const htmlOutput = htmlForSidebar.evaluate();
 	htmlOutput.setTitle("Footage Entry Tool");
@@ -7,17 +7,8 @@ function loadForm() {
 	ui.showSidebar(htmlOutput);
 }
 
-// ░░░░░░░░░▓ CREATES "AUTOMATION" > "FOOTAGE ENTRY TOOL" MENU ITEMS
-function onOpen(){
-	const ui = SpreadsheetApp.getUi();
-	const menu = ui.createMenu("Automation");
-
-	menu.addItem("Footage Entry Tool", "loadForm");
-	menu.addToUi();
-}
-
 // ░░░░░░░░░▓ SIMPLIFIES ADDING ADDITIONAL HTML FILES
 function include(filename){
 	return HtmlService.createHtmlOutputFromFile(filename)
 		.getContent();
-};
+}
